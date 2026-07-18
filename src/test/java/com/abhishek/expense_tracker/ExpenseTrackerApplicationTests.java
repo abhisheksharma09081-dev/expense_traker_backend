3aec03fd01c3dca16218331ad0e8925eef95c5b4
Abhishek-it99 <abhisheksharma999777@gmail.com>
2026-07-18 11:47:33 +0530

    Will be enabled when integration tests are added

--- src/test/java/com/abhishek/expense_tracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/com/abhishek/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -1,13 +1,14 @@
 package com.abhishek.expense_tracker;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Will be enabled when integration tests are added")
 class ExpenseTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
